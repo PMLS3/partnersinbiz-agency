a1e15c0d99dda75b3b914c1d4bbd0042615dcea8
PMLS3 <peet@partnersinbiz.tech>
2020-11-20 08:24:26 +0200

    first

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="h-screen text-white bg-black">
+    <nav class="flex flex-row-reverse space-x-4 space-x-reverse">
+      <img class="logo" src="~/static/icon.png" />
+      <h2>Schedule Now</h2>
+    </nav>
+    <div>
+      <h2>We're Partners in Biz</h2>
+      <h3>
+        We help entrepreneurs / freelancers / business owners scale and get more
+        clients by building incredible marketing systems.
+      </h3>
+      <p>We currently available for consulting</p>
+    </div>
+
+    <div>
+      <h2>
+        By challenging tradional systems, We've been able to work in the beauty,
+        fitness, health, remodeling and digital service industries.
+      </h2>
+      <p>
+        Each industry requires its own thorough market research. This means that
+        each industry has different systems in place to ensure maximum results
+      </p>
+    </div>
+
+    <div>
+      <h2>Here is what we currently offer:</h2>
+      <ul>
+        <li>Ad management</li>
+        <li>Lead generation</li>
+        <li>Funnels</li>
+        <li>Website creation/design</li>
+      </ul>
+    </div>
+    <div>Let's Get to Work. Contact us</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
