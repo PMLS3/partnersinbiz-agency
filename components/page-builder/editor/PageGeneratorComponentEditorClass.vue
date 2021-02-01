c4ef92560e1b7b8ad649f800ef45683f8857f39b
PMLS3 <peet@partnersinbiz.tech>
2021-02-01 10:09:24 +0200

    Editor Class

--- components/page-builder/editor/PageGeneratorComponentEditorClass.vue
+++ components/page-builder/editor/PageGeneratorComponentEditorClass.vue
@@ -90,11 +90,208 @@
           </vs-col>
         </vs-tab>
         <vs-tab label="Layout">
-          <ClassLayoutFloats @emitvalue="emitvalue" />
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Box Sizing</div>
+
+              <ClassLayoutBoxSizing @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Clear</div>
+
+              <ClassLayoutClear @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Container</div>
+
+              <ClassLayoutContainer @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Display</div>
+
+              <ClassLayoutDisplay @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Floats</div>
+
+              <ClassLayoutFloats @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Object Fit</div>
+
+              <ClassLayoutObjectFit @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Object Position</div>
+
+              <ClassLayoutObjectPosition @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Over flow</div>
+
+              <ClassLayoutOverflow @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Overscroll Behaviour</div>
+
+              <ClassLayoutOverscrollBehaviour @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Position</div>
+
+              <ClassLayoutPosition @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Top/Right/Left/Bottom</div>
+
+              <ClassLayoutTopRightBottomLeft @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Visibility</div>
+
+              <ClassLayoutVisibility @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Z-index</div>
+
+              <ClassLayoutZIndex @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
         </vs-tab>
-        <vs-tab label="Flexbox"> </vs-tab>
-        <vs-tab label="Grid"> </vs-tab>
-        <vs-tab label="Box Alignment"> </vs-tab>
+        <vs-tab label="Flexbox">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Flexbox</div>
+
+              <ClassFlexbox @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Flexbox Grow</div>
+
+              <ClassFlexboxGrow @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Flexbox Direction</div>
+
+              <ClassFlexboxDirection @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Flexbox Order</div>
+
+              <ClassFlexboxOrder @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Flexbox Shrink</div>
+
+              <ClassFlexboxShrink @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Flexbox Wrap</div>
+
+              <ClassFlexboxWrap @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Grid">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Grid Auto Columns</div>
+
+              <ClassGridAutoColumns @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Grid Auto Flow</div>
+
+              <ClassGridAutoFlow @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Grid Auto Rows</div>
+
+              <ClassGridAutoRows @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Grid Column Start End</div>
+
+              <ClassGridColumnStartEnd @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Grid Gap</div>
+
+              <ClassGridGap @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Grid Tamplate Rows</div>
+
+              <ClassGridTemplateRows @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Grid Row Start End</div>
+
+              <ClassGridRowStartEnd @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Grid Template Columns</div>
+
+              <ClassGridTemplateColumn @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Box Alignment">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Box Align Content</div>
+
+              <ClassBoxAlignContent @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Align Items</div>
+
+              <ClassBoxAlignItems @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Align Self</div>
+
+              <ClassBoxAlignSelf @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Justify Content</div>
+
+              <ClassBoxJustifyContent @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Justify Items</div>
+
+              <ClassBoxJustifyItems @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Justify Self</div>
+
+              <ClassBoxJustifySelf @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Place Content</div>
+
+              <ClassBoxPlaceContent @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Place Items</div>
+
+              <ClassBoxPlaceItems @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Box Place Self</div>
+
+              <ClassBoxPlaceSelf @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
         <vs-tab label="Spacing">
           <vs-collapse type="shadow">
             <vs-collapse-item>
@@ -106,17 +303,323 @@
               <div slot="header">Padding</div>
               <ClassSpacingPadding @emitvalue="emitvalue" />
             </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Space Between</div>
+              <ClassSpacingSpaceBetween @emitvalue="emitvalue" />
+            </vs-collapse-item>
           </vs-collapse>
         </vs-tab>
-        <vs-tab label="Sizing"> </vs-tab>
-        <vs-tab label="Typography"> </vs-tab>
-        <vs-tab label="Backgrounds"> </vs-tab>
-        <vs-tab label="Borders"> </vs-tab>
-        <vs-tab label="Effects"> </vs-tab>
-        <vs-tab label="Tables"> </vs-tab>
-        <vs-tab label="Transitions"> </vs-tab>
-        <vs-tab label="Transforms"> </vs-tab>
-        <vs-tab label="Interactivity"> </vs-tab>
+        <vs-tab label="Sizing">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Height</div>
+
+              <ClassSizingHeight @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Width</div>
+
+              <ClassSizingWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Max Height</div>
+
+              <ClassSizingMaxHeight @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Min Height</div>
+
+              <ClassSizingMinHeight @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Max Width</div>
+
+              <ClassSizingMaxWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Min Width</div>
+
+              <ClassSizingMinWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Typography">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Typography</div>
+
+              <ClassTypography @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Backgrounds">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Attachment</div>
+
+              <ClassBackgroundsAttachment @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Clip</div>
+
+              <ClassBackgroundsClip @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Color</div>
+
+              <ClassBackgroundsColor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Gradient Color Stops</div>
+
+              <ClassBackgroundsGradientColorStops @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Image</div>
+
+              <ClassBackgroundsImage @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Opacity</div>
+
+              <ClassBackgroundsOpacity @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Position</div>
+
+              <ClassBackgroundsPosition @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Repeat</div>
+
+              <ClassBackgroundsRepeat @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Backgrounds Size</div>
+
+              <ClassBackgroundsSize @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Borders">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Borders Color</div>
+
+              <ClassBordersColor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Divide Color</div>
+
+              <ClassBordersDivideColor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Divide Opacity</div>
+
+              <ClassBordersDivideOpacity @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Divide Style</div>
+
+              <ClassBordersDivideStyle @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Divide Width</div>
+
+              <ClassBordersDivideWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Opacity</div>
+
+              <ClassBordersOpacity @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Radius</div>
+
+              <ClassBordersRadius @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Ring Color</div>
+
+              <ClassBordersRingColor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Ring Offset Color</div>
+
+              <ClassBordersRingOffsetColor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Ring Offset Width</div>
+
+              <ClassBordersRingOffsetWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Ring Opacity</div>
+
+              <ClassBordersRingOpacity @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Ring Width</div>
+
+              <ClassBordersRingWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Style</div>
+
+              <ClassBordersStyle @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Borders Width</div>
+
+              <ClassBordersWidth @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Effects">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Effects Box Shadow</div>
+
+              <ClassEffectsBoxShadow @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Effects Opacity</div>
+
+              <ClassEffectsOpacity @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Tables">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Tables Collapse</div>
+
+              <ClassTablesCollapse @emitvalue="emitvalue" />
+            </vs-collapse-item>
+            <vs-collapse-item>
+              <div slot="header">Tables Layout</div>
+
+              <ClassTablesLayout @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Transitions">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Transitions Animation</div>
+
+              <ClassTransitionsAnimation @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transitions Delay</div>
+
+              <ClassTransitionsDelay @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transitions Duration</div>
+
+              <ClassTransitionsDuration @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transitions Property</div>
+
+              <ClassTransitionsProperty @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transitions Timing Function</div>
+
+              <ClassTransitionsTimingFunction @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Transforms">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">Transforms</div>
+
+              <ClassTransforms @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transforms Origin</div>
+
+              <ClassTransformsOrigin @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transforms Rotate</div>
+
+              <ClassTransformsRotate @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transforms Scale</div>
+
+              <ClassTransformsScale @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transforms Skew</div>
+
+              <ClassTransformsSkew @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">Transforms Translate</div>
+
+              <ClassTransformsTranslate @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
+        <vs-tab label="Interactivity">
+          <vs-collapse type="shadow">
+            <vs-collapse-item>
+              <div slot="header">InterActivity Appearance</div>
+
+              <ClassInteractivityAppearance @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">InterActivity Cursor</div>
+
+              <ClassInteractivityCursor @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">InterActivity Outline</div>
+
+              <ClassInteractivityOutline @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">InterActivity Pointer Events</div>
+
+              <ClassInteractivityPointerEvents @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">InterActivity Resize</div>
+
+              <ClassInteractivityResize @emitvalue="emitvalue" />
+            </vs-collapse-item>
+
+            <vs-collapse-item>
+              <div slot="header">InterActivity User Select</div>
+
+              <ClassInteractivityUserSelect @emitvalue="emitvalue" />
+            </vs-collapse-item>
+          </vs-collapse>
+        </vs-tab>
       </vs-tabs>
     </PerfectScrollbar>
 
